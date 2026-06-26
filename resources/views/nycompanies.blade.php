2d781667802859d0846d24a299df3cbac4c467a7
Bobby <rob.fantini@gmail.com>
2026-06-26 03:24:33 +0200

    footer change

--- resources/views/nycompanies.blade.php
+++ resources/views/nycompanies.blade.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Founding Partners</title>
+<title>Founding Partners - NewYork.dev</title>
 
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
@@ -520,10 +520,7 @@ footer{
 
 </section>
 
-
-
-
-
+ 
 <!-- FOOTER -->
 
 <footer>
@@ -531,7 +528,7 @@ footer{
     <div class="container footer-inner">
 
         <div>
-            © 2026 NY Jobs. All rights reserved.
+            © 2026 NewYork.dev. All rights reserved.
         </div>
 
         <div class="footer-links">
