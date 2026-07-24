5eed35e64bcf6346a5334a9acdf54e0da6da76b4
Bobby <rob.fantini@gmail.com>
2026-07-24 21:34:33 +0200

    video2

--- resources/views/video2.blade.php
+++ resources/views/video2.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>newyork.dev — Developer Jobs</title>
+    <title>newyork.dev — Developer Jobs - video2</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
